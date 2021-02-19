653222e7824418cd1c0d9feea1cd5b191474a4e1
Denyl <43800266+DenylR@users.noreply.github.com>
2021-02-19 14:06:14 -0500

    Agregado sumar enteros

--- ejericio1/main.hs
+++ ejericio1/main.hs
@@ -6,9 +6,14 @@ cargar = do
   putStrLn "Contenido del archivo:"
   putStrLn $ archivo 
   putStrLn "Archivo deserializado:"
-  putStrLn . show $ deserializar archivo
+  let listaInt = deserializar archivo
+  putStrLn . show $ listaInt
+  putStrLn "\nResultado:"
+  putStrLn . show $ sumarListas $ map (take (minimum $ map length listaInt)) listaInt
   cargar
-
+  where
+    sumarListas lista = foldl1 (zipWith (+)) lista
+    
 deserializar :: String -> [[Int]]
 deserializar archivo = map (map readInt) (map words (lines archivo))
   where
