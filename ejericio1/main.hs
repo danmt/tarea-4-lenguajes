189a93b1716892a73a7040613b9b2f39e884f8b1
Daniel Marin <danielarturomt@gmail.com>
2021-02-19 15:04:07 -0300

    feat(ejercicio1): agregar metodo para deserializar

--- ejericio1/main.hs
+++ ejericio1/main.hs
@@ -1,10 +1,18 @@
-cargar :: IO String
+cargar :: IO [[Int]]
 cargar = do
   putStrLn "Introduce el archivo a leer."
   nombreArchivo <- getLine
   archivo <- readFile nombreArchivo
-  putStrLn archivo
-  return archivo
+  putStrLn "Contenido del archivo:"
+  putStrLn $ archivo 
+  putStrLn "Archivo deserializado:"
+  putStrLn . show $ deserializar archivo
+  cargar
 
-main::IO String
+deserializar :: String -> [[Int]]
+deserializar archivo = map (map readInt) (map words (lines archivo))
+  where
+    readInt = read :: String -> Int
+
+main::IO [[Int]]
 main = cargar
